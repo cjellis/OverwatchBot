7fc31ce4ab35d0882207c2a30a5803c653f142e3
Andrew Knueven <knuevenam@gmail.com>
2017-04-10 21:30:11 -0400

    start of keyword generation

---
+++ keywords.py
@@ -0,0 +1,29 @@
+import nltk
+import json
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.corpus import stopwords
+
+keywords = {}
+
+with open('tokens.json') as infile:
+	tokens = json.load(infile)
+
+lemmatizer = WordNetLemmatizer()
+for char, tokenlist in tokens.items():
+	# there's another level of list here
+	# that contains each sentence. this should be removed eventually
+	for sentence in tokenlist:
+		lemmatized = [lemmatizer.lemmatize(token) for token in sentence]
+		stop = stopwords.words('english')
+		keywords[char] = [token for token in sentence if token not in stop]
+
+# now count the frequency of each word, 
+# and compute the inverse document frequency
+# and then done.
+
+res = open("keywords.json", "w")
+res.write(json.dumps(keywords))
+res.close
+
+
+
