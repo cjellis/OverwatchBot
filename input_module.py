d3da89c1e773146255b24db47c93acf0f1d5621a
Andrew Knueven <knuevenam@gmail.com>
2017-04-12 21:29:42 -0400

    normalizes input kind-of correctly

--- input_module.py
+++ input_module.py
@@ -1,6 +1,7 @@
 import nltk
 import json
 import decision_module
+from nltk.stem.wordnet import WordNetLemmatizer
 
 keywords = []
 
@@ -11,22 +12,36 @@ def read_keywords():
         for key in data:
             keywords.append(key)
 
+def load_normalized():
+    with open('normalized.json', 'r') as n:
+        data = json.load(n)
+        return data
 
 def parse(input):
+    lemmatizer = WordNetLemmatizer()
     tokenized = nltk.word_tokenize(input)
+    print(tokenized)
+    tokenized = [token.lower() for token in tokenized]
+    normalize_list = load_normalized()
+    normalized = []
+    print(normalized)
+    for key, value in normalize_list.items():
+        tokenized = [token.replace(key, value) for token in tokenized]
+    normalized = tokenized
+    print(normalized)
     local_keywords = []
     for key in keywords:
-        if key in tokenized:
+        if key in normalized:
             local_keywords.append(key)
-    return decision_module.generate_response(tokenized, local_keywords)
+    return decision_module.generate_response(normalized, local_keywords)
 
 
 def run():
     while True:
-        input = str(raw_input('What would you like to say? >'))
-        if input == 'exit':
+        user_input = str(input('What would you like to say? >'))
+        if user_input == 'exit':
             break
-        print parse(input)
+        print(parse(user_input))
 
 
 read_keywords()
