c444650a6a743af989ae6e4a679e528961d956f0
Craig Ellis <ellis.cr@husky.neu.edu>
2017-04-16 22:11:05 -0400

    finished mains, reran keyword script

--- src/generate_keywords_from_knowledge.py
+++ src/generate_keywords_from_knowledge.py
@@ -2,10 +2,12 @@ import json
 #################################################
 keywords = []
 #################################################
+responses = 0
 # Read in knowledge base and get all keywords
 with open('kb.json', 'r') as kb:
     data = json.load(kb)
     for key, value in data.items():
+        responses += len(value)
         keys = key.split(",")
         for keyword in keys:
             if keyword not in keywords:
@@ -14,3 +16,5 @@ with open('kb.json', 'r') as kb:
 # Write the keywords to the file
 with open('keywords_in_kb.json', 'w') as keys:
     keys.write(json.dumps(keywords))
+
+print(responses)
